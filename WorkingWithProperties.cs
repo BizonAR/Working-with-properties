b7e55910c5297d63263cc4d00d6dd61ea1b42266
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 14:11:23 +0500

    Working with properties

---
+++ WorkingWithProperties.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace WorkingWithProperties
+{
+	internal class WorkingWithProperties
+	{
+		static void Main()
+		{
+			Player player = new Player(8, 8, '@');
+			Renderer renderer = new Renderer();
+			renderer.Draw(player.PositionX, player.PositionY, player.PlayerSymbol);
+		}
+	}
+}
+
+class Player
+{
+	public Player(int positionX, int positionY, char playerSymbol)
+	{
+		PositionX = positionX;
+		PositionY = positionY;
+		PlayerSymbol = playerSymbol;
+	}
+
+	public int PositionX { get; private set; }
+	public int PositionY { get; private set; }
+	public char PlayerSymbol { get; private set; }
+}
+
+class Renderer
+{
+	public void Draw(int positionX, int positionY, char playerSymbol)
+	{
+		Console.SetCursorPosition(positionX, positionY);
+		Console.WriteLine(playerSymbol);
+	}
+}
